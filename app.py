2469e33b962edb664b5b731dbe538cbf6ae69b58
wdawdadwaawd-byte <wdawdadwaawd@gmail.com>
2026-03-09 23:24:07 +0300

    Add basic Tkinter GUI to app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import tkinter as tk
 
+root = tk.Tk()
+root.title("App v1.0")
+root.geometry("300x150")
+
+label = tk.Label(root, text="Merhaba dayı 😎\nBu app.py çalışıyor", font=("Arial", 12))
+label.pack(expand=True)
+
+root.mainloop()
